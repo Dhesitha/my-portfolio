3dcdc02af68e49115cda87abc5a266dd8b6dc496
Dhesitha <sithumliyanage054@gmail.com>
2026-02-02 13:35:08 +0530

    newly Fixed all layout issues and stats alignment

--- script.js
+++ script.js
@@ -46,15 +46,17 @@ window.onpageshow = function(event) {
 };
 
 var swiper = new Swiper(".mySwiper", {
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+    loop: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
     autoplay: {
-        delay: 3000,
+        delay: 2500,
+        disableOnInteraction: false,
     },
 });
